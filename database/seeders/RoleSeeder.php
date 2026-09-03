00bec6f90c33e69dd65ba1ed39f11b4c2226cc76
Lucatj <lucadegroot2008@outlook.com>
2026-09-03 16:57:36 +0200

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -95,7 +95,7 @@ class RoleSeeder extends Seeder
             ['user_id' => $testUser->id],
             [
                 'status'            => 'active',
-                'approved_by'       => 1,
+                'approved_by'       => 0,
                 'paid_contribution' => true,
             ]
         );
